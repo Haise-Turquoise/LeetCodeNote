bff67df165255daa7d0e6c413e485e1fa1ebf756
Haise-Turquoise <68551627+Haise-Turquoise@users.noreply.github.com>
2022-07-02 16:58:35 -0400

    Update leet25.cpp

--- leet25.cpp
+++ leet25.cpp
@@ -10,6 +10,7 @@
  */
 
 #include <stack>
+#include <iostream>
 using namespace std;
 class Solution {
 public:
@@ -18,6 +19,8 @@ public:
         ListNode* leftptr = nullptr;
         ListNode* rightptr = nullptr;
         ListNode* startptr = nullptr;
+        ListNode* subend = nullptr; // end of the prev sublist
+        ListNode* alphaptr = nullptr;
         stack<int> bin;
         stack<int> bin2;
         if (k == 1) return head;
@@ -26,52 +29,52 @@ public:
         int cnt = 0;
         
         while (curptr != nullptr) {
-            if (cnt == k) break;
             cnt++;
             bin.push(curptr->val);
+            if (cnt == k) {
+                while (!bin.empty()) {
+                    // create new node with val from top of bin
+                    int curval = bin.top(); bin.pop();
+                    ListNode* nd = new ListNode(curval);
+                    if (alphaptr == nullptr) alphaptr = nd; // first node of returned list
+                    // link the previously created node
+                    if (startptr == nullptr) { // record first node pos
+                        startptr = nd;
+                    } else {
+                        leftptr->next = nd;
+                    }
+                    leftptr = nd;
+                }
+                // link prev part
+                if (subend != nullptr) {
+                    subend->next = startptr;  // previous end subend link to current sublist start position startptr
+                } 
+                subend = leftptr;         // subend update to current sublist end
+                cnt = 0;
+                startptr = nullptr;
+                leftptr = nullptr;
+            }
             curptr = curptr->next;
         }
-        
-        
-        
-        
-        
-        // create reverse sublist and link to previous 
-        if (cnt == k) {
-            // curptr is on the start of the next sublist
-            
-            while (!bin.empty()) {
-                // create new node with val from top of bin
-                int curval = bin.top();
-                bin.pop();
-                ListNode* nd = ListNode(curval);
-                // link the previously created node
-                if (startptr == nullptr) {
-                    startptr = nd;
-                } else {
-                    leftptr->next = nd;
-                }
-                leftptr = nd;
-            }
-            // link prev part
-            if (subend != nullptr) {
-                subend->next = startptr;  // previous end subend link to current sublist start position startptr
-                subend = leftptr;         // subend update to current sublist end
-            }
-        } else {//if counter not reach but nullptr found, throw to another bin and restore
+        // restore rest if remained part is not enough
+        if (cnt != k) {
+            //if counter not reach but nullptr found, throw to another bin and restore
+            cout << "bin2 content: " ;
             while (!bin.empty()) {
                 int curval = bin.top();
                 bin.pop();
                 bin2.push(curval);
+                cout << curval << " ";
             }
             leftptr = nullptr;
             while (!bin2.empty()) {
                 // create new node with val from top of bin2
-                int curval = bin2.top();
-                bin2.pop();
-                ListNode* nd = ListNode(curval);
+                int curval = bin2.top(); bin2.pop();
+                ListNode* nd = new ListNode(curval);
                 // link the previously created node
-                if (leftptr != nullptr) {
+                if (startptr == nullptr) { // record first node pos
+                    startptr = nd;
+                } else {
                     leftptr->next = nd;
                 }
                 leftptr = nd;
@@ -79,16 +82,6 @@ public:
             // link to prev sublist 
             subend->next = startptr;  // previous end subend link to current sublist start position startptr
         }
-        cnt == 0;
-        
-            
-        
-        
-        
-        
-        
-        
-        
-        
+        return alphaptr;
     }
 };
