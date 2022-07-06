f9f4e8dc2e4e9bb3e22f56bb7de5c8c91d5f163f
Haise-Turquoise <68551627+Haise-Turquoise@users.noreply.github.com>
2022-07-06 07:45:41 -0400

    Update leet1.cpp

--- leet1.cpp
+++ leet1.cpp
@@ -1,19 +1,37 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    int firstUniqChar(string s) {
-        bool exist = true;
-        for ( int i = 0; i < s.size(); i++ ) {
-            exist = true;
-            for ( int j = 0; j < s.size(); j++ ) {
-                if (i!=j) {
-                    if ( s.at(i) == s.at(j) ) {
-                        exist = false;
-                        break;
-                    }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ret;
+        unordered_map<int,int> umap;
+        
+        for (size_t i = 0; i < nums.size(); i++) {
+            int key = target - nums[i];
+            if (umap.find(key) != umap.end()) {
+                
+                ret.push_back(  umap.at( key )  );
+                ret.push_back(i);
+            } else {
+                umap.insert( make_pair(nums[i],i) );
+            }
+        }
+        return ret;
+        
+        
+        /*brute
+        vector<int> ret;
+        for ( int i = 0 ; i < nums.size() ; i++ ) {
+            for ( int j = i+1 ; j < nums.size() ; j++ ) {
+                if (nums.at(i) + nums.at(j) == target) {
+                    ret.push_back(i);
+                    ret.push_back(j);
                 }
             }
-            if (exist) return i;
         }
-        return -1;
+        return ret;
+        */
     }
 };
